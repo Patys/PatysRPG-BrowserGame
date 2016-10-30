93124a3420ab5e9396e0561ca2b3029d8e927c29
Patryk Szczygło <patysprog@gmail.com>
2016-10-30 17:14:53 +0100

    added router login

---
+++ app/login.js
@@ -0,0 +1,36 @@
+var mysql = require('mysql');
+var pool = mysql.createPool({
+    host     : process.env.OPENSHIFT_MYSQL_DB_HOST,
+    user     : process.env.OPENSHIFT_MYSQL_DB_USERNAME,
+    password : process.env.OPENSHIFT_MYSQL_DB_PASSWORD,
+    port     : process.env.OPENSHIFT_MYSQL_DB_PORT,
+    database : process.env.OPENSHIFT_APP_NAME
+  });
+
+module.exports.get = function (req, res) {
+  res.render('login', { title: 'Logowanie', message: 'Logujemy sie'});
+}
+
+module.exports.post = function (req, res) {
+  var post = req.body;
+  var query = "SELECT users.name, users.password, users.token FROM users";
+  //WHERE users.name=" + post.username + " AND users.password=" + post.password + "";
+  pool.getConnection(function(err, conn){
+    if(err) {
+      res.send('Bad user/passw');
+    } else {
+      conn.query(query, function(err, rows) {
+        if(err) {
+          res.send('Bad user/passw');
+        } else {
+          if(post.username === rows.name && post.password === rows.password) {
+            req.session.user_id = rows.token;
+            res.redirect('/game');
+          }else {
+            res.send('Bad user/passw');
+          }
+        }
+      });
+    }
+  });
+}
--- app/routes.js
+++ app/routes.js
@@ -2,7 +2,10 @@ var express = require('express');
 var routes = express.Router();
 
 var index = require('./index');
+var login = require('./login');
 
 routes.get('/', index);
+routes.get('/login', login.get);
+routes.get('/login', login.post);
 
 module.exports = routes;
