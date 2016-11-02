35f816ff32877281d9615db49b69e93f7256cdb3
Patys <patys.prog@gmail.com>
2016-11-02 11:35:51 +0100

    fix session

--- server.js
+++ server.js
@@ -16,15 +16,15 @@ var routes = require('./app/routes');
 app.set('view engine', 'pug');
 app.set('views', './public/views');
 app.set('trust proxy', 1);
-app.use(session({
-  secret: 'bardzo tajne i dkugie haslo',
-  // resave: false,
-  // saveUninitialized: true,
-  cookie: {
-    path    : '/',
-    // secure: true
-  }
-}));
+// app.use(session({
+//   secret: 'bardzo tajne i dkugie haslo',
+//   // resave: false,
+//   // saveUninitialized: true,
+//   cookie: {
+//     path    : '/',
+//     // secure: true
+//   }
+// }));
 
 app.use('/', routes);
 
