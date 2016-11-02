d765d5b8d4fd9fce88069c7a74138fd9b5ab9b2b
Patys <patys.prog@gmail.com>
2016-11-02 11:21:11 +0100

    debug auth

--- app/routes.js
+++ app/routes.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json({ type: 'application/json'});
 var routes = express.Router();
 
 var auth = require('./auth');
@@ -6,9 +8,15 @@ var index = require('./index');
 var login = require('./login');
 var game = require('./game');
 
+
+routes.use(function(req, res, next) {
+    // log each request to the console
+    console.log(req.method, req.url);
+    next();
+});
 routes.get('/', index);
 routes.get('/login', login.get);
-routes.post('/login', login.post);
+routes.post('/login', jsonParser, login.post);
 routes.get('/game', auth.check, game.get);
 
 module.exports = routes;
