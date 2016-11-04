ed348888b7dca81fd0d6a2730e3b4b13c54936e6
Patryk Szczygło <patysprog@gmail.com>
2016-11-04 22:24:33 +0100

    fix register

--- app/dbhelper.js
+++ app/dbhelper.js
@@ -1,5 +1,5 @@
 module.exports.query = function (query, args, conn, next) {
-  if(args != '') {
+  if(args) {
     conn.query(query, args,function(err, rows) {
       if(err) throw err;
       next(rows);
