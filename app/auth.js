c40c627f5d73bcce25bf78c4cb30cd2631fe914f
Patryk Szczygło <patysprog@gmail.com>
2016-11-04 19:33:24 +0100

    changed color of jumbotron

--- app/auth.js
+++ app/auth.js
@@ -1,7 +1,7 @@
 
 module.exports.check = function (req, res, next) {
   if (!req.session.user_id) {
-    res.send('You are not authorized to viewtest this page');
+    res.redirect('/login');
   } else {
     // Moze wywalac, wymusza rerender
     //res.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
