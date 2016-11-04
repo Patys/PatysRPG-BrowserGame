3a61fce0735c3e7ee75ca64018a619646ccf1fde
Patryk Szczygło <patysprog@gmail.com>
2016-11-04 21:04:55 +0100

    refactored register

--- app/register.js
+++ app/register.js
@@ -36,19 +36,41 @@ module.exports.post = function (req, res) {
                 ('00' + date.getUTCHours()).slice(-2) + ':' +
                 ('00' + date.getUTCMinutes()).slice(-2) + ':' +
                 ('00' + date.getUTCSeconds()).slice(-2);
-            var queryInsert = "INSERT INTO users VALUES ('',\""+post.username+"\",\""+post.password+"\",\""+post.email+"\",\""+date+"\",\""+guid()+"\");";
-            conn.query(queryInsert, function(err, rows) {
-              if(err) {
-                res.json(err);
-                // res.send('Bad user/passw');
-              } else {
+            var queryInsertUser = "INSERT INTO users VALUES ('',\""+post.username+"\",\""+post.password+"\",\""+post.email+"\",\""+date+"\",\""+guid()+"\");";
+            insertQuery(queryInsertUser, conn, function(result) {
+              var queryInsertCharacter = "INSERT INTO characters VALUES ('',\""+result.insertId+"\",\"5\",\"5\",\"5\",\"5\");";
+              insertQuery(queryInsertCharacter, conn, function(res1) {
                 res.redirect('/login');
-              }
+              });
             });
+
+            // conn.query(queryInsertUser, function(err, rows) {
+            //   if(err) {
+            //     res.json(err);
+            //     // res.send('Bad user/passw');
+            //   } else {
+            //     var queryInsertCharacter = "INSERT INTO characters VALUES ('',\""+rows.insertId+"\",\"5\",\"5\",\"5\",\"5\");";
+            //     conn.query(queryInsertCharacter, function(err, rows) {
+            //       if(err) {
+            //         res.json(err);
+            //         // res.send('Bad user/passw');
+            //       } else {
+            //         res.redirect('/login');
+            //       }
+            //     });
+            //   }
+            // });
           }
         }
       });
     }
+  });
+}
+
+function insertQuery(query, conn, next) {
+  conn.query(query, function(err, rows) {
+    if(err) throw err;
+    next(rows);
   });
 }
 
