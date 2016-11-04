19854da8399cf4ef32e4c03fa73d81ce5e3446fc
Patryk Szczygło <patysprog@gmail.com>
2016-11-04 21:42:57 +0100

    refactored db query and added game character stats

--- app/game.js
+++ app/game.js
@@ -6,16 +6,34 @@ var pool = mysql.createPool({
     port     : process.env.OPENSHIFT_MYSQL_DB_PORT,
     database : process.env.OPENSHIFT_APP_NAME
   });
+var db = require('./dbhelper');
 
-var json_game_data = {
-  strength: 5,
-  vitality: 4,
-  inteligence: 2,
-  agility: 666
-
-};
+function getData(req) {
+  var query = 'SELECT id FROM `users` WHERE `token` = ? ';
+  //WHERE users.name=" + post.username + " AND users.password=" + post.password + "";
+  pool.getConnection(function(err, conn){
+    if(err) throw err;
+    db.query(query, [req.session.user_id], conn, function(result) {
+      if(result[0]) {
+        var selectStats = 'SELECT * from characters WHERE "id_user" = ?';
+        db.query(selectStats, [result[0].id], conn, function(res1) {
+          if(res1[0]) {
+            var json_data = {
+              strength: res1[0].strength,
+              vitality: res1[0].vitality,
+              inteligence: res1[0].inteligence,
+              agility: res1[0].agility
+            };
+            return json_data;
+          }
+        });
+      }
+    });
+  });
+}
 
 module.exports.get = function (req, res) {
+  var json_game_data = getData(req);
   res.render('game', json_game_data);
 
 }
--- app/register.js
+++ app/register.js
@@ -6,6 +6,7 @@ var pool = mysql.createPool({
     port     : process.env.OPENSHIFT_MYSQL_DB_PORT,
     database : process.env.OPENSHIFT_APP_NAME
   });
+var db = require('./dbhelper');
 
 module.exports.get = function (req, res) {
   res.render('register');
@@ -20,57 +21,31 @@ module.exports.post = function (req, res) {
       res.json(err);
       // res.send('Bad user/passw');
     } else {
-      conn.query(query, [post.username, post.email], function(err, rows) {
-        if(err) {
-          res.json(err);
-          // res.send('Bad user/passw');
+      db.query(query, [post.username, post.email], function(rows) {
+        if(rows[0]) {
+          res.send('Uzytkownik istnieje');
         } else {
-          if(rows[0]) {
-            res.send('Uzytkownik istnieje');
-          } else {
-            var date;
-            date = new Date();
-            date = date.getUTCFullYear() + '-' +
-                ('00' + (date.getUTCMonth()+1)).slice(-2) + '-' +
-                ('00' + date.getUTCDate()).slice(-2) + ' ' +
-                ('00' + date.getUTCHours()).slice(-2) + ':' +
-                ('00' + date.getUTCMinutes()).slice(-2) + ':' +
-                ('00' + date.getUTCSeconds()).slice(-2);
-            var queryInsertUser = "INSERT INTO users VALUES ('',\""+post.username+"\",\""+post.password+"\",\""+post.email+"\",\""+date+"\",\""+guid()+"\");";
-            insertQuery(queryInsertUser, conn, function(result) {
-              var queryInsertCharacter = "INSERT INTO characters VALUES ('',\""+result.insertId+"\",\"5\",\"5\",\"5\",\"5\");";
-              insertQuery(queryInsertCharacter, conn, function(res1) {
+          var date;
+          date = new Date();
+          date = date.getUTCFullYear() + '-' +
+              ('00' + (date.getUTCMonth()+1)).slice(-2) + '-' +
+              ('00' + date.getUTCDate()).slice(-2) + ' ' +
+              ('00' + date.getUTCHours()).slice(-2) + ':' +
+              ('00' + date.getUTCMinutes()).slice(-2) + ':' +
+              ('00' + date.getUTCSeconds()).slice(-2);
+          var queryInsertUser = "INSERT INTO users VALUES ('',\""+post.username+"\",\""+post.password+"\",\""+post.email+"\",\""+date+"\",\""+guid()+"\");";
+          db.query(queryInsertUser,'', conn, function(result) {
+            var queryInsertCharacter = "INSERT INTO characters VALUES ('',\""+result.insertId+"\",\"5\",\"5\",\"5\",\"5\");";
+            db.query(queryInsertCharacter,'', conn, function(res1) {
+              var queryInsertEquipment = "INSERT INTO equipment VALUES ('',\""+result.insertId+"\",\"0\",\"0\",\"0\",\"0\",\"0\",\"0\",\"0\",\"0\",\"0\",\"0\");";
+              db.query(queryInsertEquipment,'', conn, function(res2) {
                 res.redirect('/login');
               });
             });
-
-            // conn.query(queryInsertUser, function(err, rows) {
-            //   if(err) {
-            //     res.json(err);
-            //     // res.send('Bad user/passw');
-            //   } else {
-            //     var queryInsertCharacter = "INSERT INTO characters VALUES ('',\""+rows.insertId+"\",\"5\",\"5\",\"5\",\"5\");";
-            //     conn.query(queryInsertCharacter, function(err, rows) {
-            //       if(err) {
-            //         res.json(err);
-            //         // res.send('Bad user/passw');
-            //       } else {
-            //         res.redirect('/login');
-            //       }
-            //     });
-            //   }
-            // });
-          }
+          });
         }
       });
     }
-  });
-}
-
-function insertQuery(query, conn, next) {
-  conn.query(query, function(err, rows) {
-    if(err) throw err;
-    next(rows);
   });
 }
 
