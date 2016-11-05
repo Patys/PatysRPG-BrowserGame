2e705ff9d0b893b3a5b81032fd817f968b1e0171
Patryk Szczygło <patysprog@gmail.com>
2016-11-05 21:06:34 +0100

    no drawing, just ready art

--- public/game.js
+++ public/game.js
@@ -29,31 +29,8 @@ var main = function() {
     $('div[id^="panel-"]').hide();
     $('div[id^="panel-group"]').show();
   });
-  drawCharacter();
-
+  
 }
 
-function drawCharacter() {
-  var c = document.getElementById("myCanvas");
-  var ctx = c.getContext("2d");
-  ctx.globalCompositeOperation='destination-over';
-
-  var img_face1 = new Image();
-  var img_faceFull1 = new Image();
-  var img_hair1 = new Image();
-
-  img_faceFull1.onload = function () {
-      ctx.drawImage(img_faceFull1, 150, 100);
-      img_face1.onload = function () {
-          ctx.drawImage(img_face1, 150, 100);
-      };
-      img_hair1.onload = function () {
-          ctx.drawImage(img_hair1, 135, 30);
-      };
-  };
-  img_face1.src = "https://github.com/Patys/PatysRPG-graphic/raw/master/Face/Completes/face1.png";
-  img_faceFull1.src = "https://github.com/Patys/PatysRPG-graphic/raw/master/Skin/Tint%201/tint1_head.png";
-  img_hair1.src = "https://github.com/Patys/PatysRPG-graphic/raw/master/Hair/Black/blackMan1.png";
-}
 
 $(document).ready(main);
