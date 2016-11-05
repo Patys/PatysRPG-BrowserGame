9ec27f6e1af4f4d273026b37f840249e1dc818f0
Patryk Szczygło <patysprog@gmail.com>
2016-11-05 17:14:22 +0100

    debug mission

--- app/game.js
+++ app/game.js
@@ -21,6 +21,7 @@ function getData(req, next) {
             var queryMissions = 'SELECT * FROM missions AS r1 JOIN (SELECT CEIL(RAND() * (SELECT MAX(id) FROM missions)) AS id) AS r2 WHERE r1.id >= r2.id ORDER BY r1.id ASC LIMIT 3';
             db.query(queryMissions, '', conn, function(res2) {
               if(res2[0]) {
+                console.dir(res2);
                   var game_data = {
                     strength: res1[0].strength,
                     vitality: res1[0].vitality,
