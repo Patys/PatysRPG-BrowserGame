cd4147574f4bfc7314abe6e2f6861eb81487a071
Patryk Szczygło <patysprog@gmail.com>
2016-11-15 17:50:18 +0100

    fixed link game

--- public/game.js
+++ public/game.js
@@ -1,36 +1,36 @@
-var main = function() {
-  $('div[id^="panel-"]').hide();
-  $('div[id^="panel-character"]').show();
-  $('#menu-character').click(function() {
-    $('div[id^="panel-"]').hide();
-    $('div[id^="panel-character"]').show();
-  });
-  $('#menu-mission').click(function() {
-    $('div[id^="panel-"]').hide();
-    $('div[id^="panel-mission"]').show();
-  });
-  $('#menu-shop').click(function() {
-    $('div[id^="panel-"]').hide();
-    $('div[id^="panel-shop"]').show();
-  });
-  $('#menu-arena').click(function() {
-    $('div[id^="panel-"]').hide();
-    $('div[id^="panel-arena"]').show();
-  });
-  $('#menu-ranking').click(function() {
-    $('div[id^="panel-"]').hide();
-    $('div[id^="panel-ranking"]').show();
-  });
-  $('#menu-messages').click(function() {
-    $('div[id^="panel-"]').hide();
-    $('div[id^="panel-messages"]').show();
-  });
-  $('#menu-group').click(function() {
-    $('div[id^="panel-"]').hide();
-    $('div[id^="panel-group"]').show();
-  });
-  
-}
-
-
-$(document).ready(main);
+// var main = function() {
+//   $('div[id^="panel-"]').hide();
+//   $('div[id^="panel-character"]').show();
+//   $('#menu-character').click(function() {
+//     $('div[id^="panel-"]').hide();
+//     $('div[id^="panel-character"]').show();
+//   });
+//   $('#menu-mission').click(function() {
+//     $('div[id^="panel-"]').hide();
+//     $('div[id^="panel-mission"]').show();
+//   });
+//   $('#menu-shop').click(function() {
+//     $('div[id^="panel-"]').hide();
+//     $('div[id^="panel-shop"]').show();
+//   });
+//   $('#menu-arena').click(function() {
+//     $('div[id^="panel-"]').hide();
+//     $('div[id^="panel-arena"]').show();
+//   });
+//   $('#menu-ranking').click(function() {
+//     $('div[id^="panel-"]').hide();
+//     $('div[id^="panel-ranking"]').show();
+//   });
+//   $('#menu-messages').click(function() {
+//     $('div[id^="panel-"]').hide();
+//     $('div[id^="panel-messages"]').show();
+//   });
+//   $('#menu-group').click(function() {
+//     $('div[id^="panel-"]').hide();
+//     $('div[id^="panel-group"]').show();
+//   });
+//
+// }
+//
+//
+// $(document).ready(main);
