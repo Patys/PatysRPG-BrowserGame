e3e896e4fcbe1b15de2a5cc084a9b676efcc6752
Patryk Szczygło <patysprog@gmail.com>
2016-11-17 20:57:33 +0100

    added js to login

--- public/register.js
+++ public/register.js
@@ -1,15 +1,19 @@
-  // jquery transit is used to handle the animation
-   $('input').focusin(function() {
-        $('label').transition({x:'80px'},500,'ease').next()
-        .transition({x:'5px'},500, 'ease');
+
+var main = function() {
+// jquery transit is used to handle the animation
+ $('input').focusin(function() {
+      $('label').transition({x:'80px'},500,'ease').next()
+      .transition({x:'5px'},500, 'ease');
 //setTimeout needed for Chrome, for some reson there is no animation from left to right, the pen is immediately present. Slight delay to adding the animation class fixes it
-         setTimeout(function(){
-		    $('label').next().addClass('move-pen');
-	      },100);
+       setTimeout(function(){
+      $('label').next().addClass('move-pen');
+      },100);
 
-		});
+  });
 
-		  $('input').focusout(function() {
-          $('label').transition({x:'0px'},500,'ease').next()
-           .transition({x:'-100px'},500, 'ease').removeClass('move-pen');
-		  });
+    $('input').focusout(function() {
+        $('label').transition({x:'0px'},500,'ease').next()
+         .transition({x:'-100px'},500, 'ease').removeClass('move-pen');
+    });  
+}
+$(document).ready(main);
