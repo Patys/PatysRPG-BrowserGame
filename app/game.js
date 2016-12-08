3e134f12287d5918fc8b386edbc2e0d7000ebc8b
Patryk Szczygło <patysprog@gmail.com>
2016-12-08 21:29:28 +0100

    added catching errors

--- app/game.js
+++ app/game.js
@@ -8,111 +8,147 @@ var pool = mysql.createPool({
   });
 var data = require('./prepareData');
 
-function getData(req, next) {
-  //WHERE users.name=" + post.username + " AND users.password=" + post.password + "";
-  pool.getConnection(function(err, conn){
-    if(err) throw err;
-    data.get(conn, req, next);
-  });
-}
+// function getData(req, next) {
+//   //WHERE users.name=" + post.username + " AND users.password=" + post.password + "";
+//   pool.getConnection(function(err, conn){
+//     if(err) throw err;
+//     data.get(conn, req, next);
+//   });
+// }
 
 module.exports.get = function (req, res) {
-  var game_data = {};
-  pool.getConnection(function(err, conn){
-    if(err) throw err;
-    data.getMessageCount(conn,req, function(messages_count){
-      res.render('game', {'messages_count': messages_count, 'game_data': game_data});
+  try {
+    var game_data = {};
+    pool.getConnection(function(err, conn){
+      if(err) throw err;
+      data.getMessageCount(conn,req, function(messages_count){
+        res.render('game', {'messages_count': messages_count, 'game_data': game_data});
+      });
     });
-  });
+  } catch (err) {
+    // handle the error safely
+    console.log(err)
+  }
 }
 
 module.exports.arena = function (req, res) {
-  var game_data = {};
-  pool.getConnection(function(err, conn){
-    if(err) throw err;
-    data.getMessageCount(conn,req, function(messages_count){
-      if (typeof messages_count !== 'undefined') {
+  try {
+    var game_data = {};
+    pool.getConnection(function(err, conn){
+      if(err) throw err;
+      data.getMessageCount(conn,req, function(messages_count){
         game_data.currentUrl = '/arena';
         res.render('game/arena', {'messages_count': messages_count, 'game_data': game_data});
-      } else {
-        res.redirect('/error');
-      }
+      });
     });
-  });
+  } catch (err) {
+    // handle the error safely
+    console.log(err)
+  }
 }
 
 module.exports.character = function (req, res) {
-  pool.getConnection(function(err, conn){
-    if(err) throw err;
-    data.getDataCharacter(conn, req, function(game_data) {
-      data.getMessageCount(conn,req, function(messages_count){
-        game_data.currentUrl = '/character';
-        res.render('game/character', {'messages_count': messages_count, 'game_data': game_data});
+  try {
+    pool.getConnection(function(err, conn){
+      if(err) throw err;
+      data.getDataCharacter(conn, req, function(game_data) {
+        data.getMessageCount(conn,req, function(messages_count){
+          game_data.currentUrl = '/character';
+          res.render('game/character', {'messages_count': messages_count, 'game_data': game_data});
+        });
       });
     });
-  });
+  } catch (err) {
+    // handle the error safely
+    console.log(err)
+  }
 }
 
 module.exports.group = function (req, res) {
-  var game_data = {};
-  pool.getConnection(function(err, conn){
-    if(err) throw err;
-    data.getMessageCount(conn,req, function(messages_count){
-      game_data.currentUrl = '/group';
-      res.render('game/group', {'messages_count': messages_count, 'game_data': game_data});
+  try {
+    var game_data = {};
+    pool.getConnection(function(err, conn){
+      if(err) throw err;
+      data.getMessageCount(conn,req, function(messages_count){
+        game_data.currentUrl = '/group';
+        res.render('game/group', {'messages_count': messages_count, 'game_data': game_data});
+      });
     });
-  });
+  } catch (err) {
+    // handle the error safely
+    console.log(err)
+  }
 }
 
 module.exports.messages = function (req, res) {
-  var page = req.query.page;
-  if(!page)
-    page = 1;
-  pool.getConnection(function(err, conn){
-    if(err) throw err;
-    data.getDataMessage(conn, req, page, function(game_data) {
-      data.getMessageCount(conn,req, function(messages_count){
-        game_data.currentUrl = '/messages';
-        res.render('game/messages', {'messages_count': messages_count, 'game_data': game_data});
+  try {
+    var page = req.query.page;
+    if(!page)
+      page = 1;
+    pool.getConnection(function(err, conn){
+      if(err) throw err;
+      data.getDataMessage(conn, req, page, function(game_data) {
+        data.getMessageCount(conn,req, function(messages_count){
+          game_data.currentUrl = '/messages';
+          res.render('game/messages', {'messages_count': messages_count, 'game_data': game_data});
+        });
       });
     });
-  });
+  } catch (err) {
+    // handle the error safely
+    console.log(err)
+  }
 }
 
 module.exports.missions = function (req, res) {
-  pool.getConnection(function(err, conn){
-    if(err) throw err;
-    data.getDataMissions(conn, req, function(game_data) {
-      data.getMessageCount(conn,req, function(messages_count){
-        game_data.currentUrl = '/missions';
-        res.render('game/missions', {'messages_count': messages_count, 'game_data': game_data});
+  try {
+    pool.getConnection(function(err, conn){
+      if(err) throw err;
+      data.getDataMissions(conn, req, function(game_data) {
+        data.getMessageCount(conn,req, function(messages_count){
+          game_data.currentUrl = '/missions';
+          res.render('game/missions', {'messages_count': messages_count, 'game_data': game_data});
+        });
       });
     });
-  });
+  } catch (err) {
+    // handle the error safely
+    console.log(err)
+  }
 }
 
 module.exports.ranking = function (req, res) {
-  var page = req.query.page;
-  if(!page)
-    page = 1;
-  pool.getConnection(function(err, conn){
-    if(err) throw err;
-    data.getDataRanking(conn, req, page, function(game_data) {
-      data.getMessageCount(conn,req, function(messages_count){
-        game_data.currentUrl = '/ranking';
-        res.render('game/ranking', {'messages_count': messages_count, 'game_data': game_data});
+  try {
+    var page = req.query.page;
+    if(!page)
+      page = 1;
+    pool.getConnection(function(err, conn){
+      if(err) throw err;
+      data.getDataRanking(conn, req, page, function(game_data) {
+        data.getMessageCount(conn,req, function(messages_count){
+          game_data.currentUrl = '/ranking';
+          res.render('game/ranking', {'messages_count': messages_count, 'game_data': game_data});
+        });
       });
     });
-  });
+  } catch (err) {
+    // handle the error safely
+    console.log(err)
+  }
 }
 
 module.exports.shop = function (req, res) {
-  var game_data = {};
-  pool.getConnection(function(err, conn){
-    if(err) throw err;
-    data.getMessageCount(conn,req, function(messages_count){
-      game_data.currentUrl = '/shop';
-      res.render('game/shop', {'messages_count': messages_count, 'game_data': game_data});
+  try {
+    var game_data = {};
+    pool.getConnection(function(err, conn){
+      if(err) throw err;
+      data.getMessageCount(conn,req, function(messages_count){
+        game_data.currentUrl = '/shop';
+        res.render('game/shop', {'messages_count': messages_count, 'game_data': game_data});
+      });
     });
-  });
+  } catch (err) {
+    // handle the error safely
+    console.log(err)
+  }
 }
