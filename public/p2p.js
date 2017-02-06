acc062b46077fbfabefc735510809dd1dc46aead
Patryk Szczygło <patysprog@gmail.com>
2017-02-06 22:18:25 +0100

    p2p and phaser

--- public/p2p.js
+++ public/p2p.js
@@ -1,4 +1,9 @@
 var peer = new Peer('', {host: 'localhost', port: 8080, path: '/p2papi'});
+var peer = new Peer({
+	host: location.hostname,
+	port: location.port || (location.protocol === 'https:' ? 443 : 80),
+	path: '/p2papi'
+});
 
 peer.on('open', function(id) {
   console.log('My peer ID is: ' + id);
