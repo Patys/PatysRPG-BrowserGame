5cf7aa54ef62c238235f4e6e27fa0dbd23502ab9
Patryk Szczygło <patysprog@gmail.com>
2017-05-23 14:06:23 +0200

    added p2p file with client logic

--- public/p2p.js
+++ public/p2p.js
@@ -1,6 +1,4 @@
-var nick = console.prompt('podaj nick');
-
-var peer = new Peer(nick ,{
+var peer = new Peer({
 	host: location.hostname,
 	port: location.port || (location.protocol === 'https:' ? 443 : 80),
 	path: '/p2papi'
